311cfab0d184be67116b6f4d13b33b02e94361e2
Piotr Pepliński <p.peplaj@gmail.com>
2023-06-25 17:11:50 +0200

    koniec 25.06

--- newspaperREST/news/views.py
+++ newspaperREST/news/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+
+def list_create_articles(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse({'articles': serializer.data})
+    else:
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
