1c3d0fde39aea32984b541f5948921b7470b7a20
Piotr Pepliński <p.peplaj@gmail.com>
2023-07-01 10:40:03 +0200

    coffee break

--- newspaperREST/news/urls.py
+++ newspaperREST/news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('articles/', views.list_create_articles)
+    path('articles/', views.list_create_articles),
+    path('articles/<int:articleId>/', views.article_detail),
 ]
--- newspaperREST/news/views.py
+++ newspaperREST/news/views.py
@@ -1,18 +1,42 @@
-from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from .models import Article
 from .serializers import ArticleSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
 
 
+@api_view(['GET', 'POST'])
 def list_create_articles(request):
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
-        return JsonResponse({'articles': serializer.data})
+        return Response(serializer.data)
     else:
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT'])
+def article_detail(request, articleId):
+    # try:
+    #     article = Article.objects.get(id=articleId)
+    # except Article.DoesNotExist:
+    #     return Response({'Error': f'Article with id {articleId} was not found.'},status=status.HTTP_404_NOT_FOUND)
+    article = get_object_or_404(Article, id=articleId)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    else:
+        serializer = ArticleSerializer(data=request.data, instance=article)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
