17e4520101e544192de7a08445fca2040ce8acca
Piotr Pepliński <p.peplaj@gmail.com>
2023-07-02 12:15:10 +0200

    added permissions

--- newspaperREST/news/views.py
+++ newspaperREST/news/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
+from rest_framework import permissions
 
 # Create your views here.
 
@@ -20,6 +21,11 @@ class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
+
+# list - IsAdmin, create - AllowAny
 class ListCreateUsers(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        return [permissions.IsAdminUser()] if self.request.method == 'GET' else [permissions.AllowAny()]
